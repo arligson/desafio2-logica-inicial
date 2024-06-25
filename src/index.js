2736f8f235a5b25426e7148b631db365828097dc
Arligson <arligson.souza@gmail.com>
2024-06-25 12:33:14 -0300

    versao 1.0

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-/**
- * 
-  # 2️⃣ Calculadora de partidas Rankeadas
-**O Que deve ser utilizado**
+let rankingDoPersonagem
 
-- Variáveis
-- Operadores
-- Laços de repetição
-- Estruturas de decisões
-- Funções
+function ranking (vitorias = 80, derrotas = 3){
+   saldoDasRankeadas = vitorias - derrotas
 
-## Objetivo:
-
-Crie uma função que recebe como parâmetro a quantidade de vitórias e derrotas de um jogador,
-depois disso retorne o resultado para uma variável, o saldo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
-
-Se vitórias for menor do que 10 = Ferro
-Se vitórias for entre 11 e 20 = Bronze
-Se vitórias for entre 21 e 50 = Prata
-Se vitórias for entre 51 e 80 = Ouro
-Se vitórias for entre 81 e 90 = Diamante
-Se vitórias for entre 91 e 100= Lendário
-Se vitórias for maior ou igual a 101 = Imortal
-
-## Saída
-
-Ao final deve se exibir uma mensagem:
-"O Herói tem de saldo de **{saldoVitorias}** está no nível de **{nivel}**"
-
- */
+    if (saldoDasRankeadas < 10){
+        return  "Ferro"
+    }
+    else if (saldoDasRankeadas >= 11 && saldoDasRankeadas<=20) {
+        return  "Bronze"
+    }
+    else if (saldoDasRankeadas >= 21 && saldoDasRankeadas<=50) {
+        return  "Prata"
+    }
+    else if (saldoDasRankeadas >= 51 && saldoDasRankeadas<=80) {
+        return  "Ouro"
+    }
+    else if (saldoDasRankeadas >= 81 && saldoDasRankeadas<=90) {
+        return  "Diamante"
+    }
+    else if (saldoDasRankeadas >= 91 && saldoDasRankeadas<=100) {
+        return  "Lendário"
+    }
+    else {
+        return  "Imortal"
+    }
+        
+}
+rankingDoPersonagem = ranking()
+console.log(`O héroi tem um saldo de ${saldoDasRankeadas} vitórias e está no nível ${rankingDoPersonagem}`)
